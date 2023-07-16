2594c027f54e82bcd4760d5a21ad74407b7c330e
= <=>
2023-07-16 15:31:24 +0800

    change redis bind ip to 0.0.0.0

--- haproxy/3.sh
+++ haproxy/3.sh
@@ -5,6 +5,7 @@ yum install -y redis
 
 # 配置 redis
 sed -i 's/# requirepass foobared/requirepass P@ssw0rd/g' /etc/redis.conf
+sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis.conf
 
 # 启动 redis
 systemctl enable redis
