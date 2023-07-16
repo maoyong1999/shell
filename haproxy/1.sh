d30ab4ea04a76557bbb0cefbf51f25e1bc48575b
= <=>
2023-07-16 14:39:53 +0800

    重写shell

--- haproxy/1.sh
+++ haproxy/1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# 提示输入虚拟IP
+read -p "请输入haproxy的虚拟IP（默认为192.168.100.101）：" virtual_ip
+virtual_ip=${virtual_ip:-192.168.100.101}
+
 # 安装 haproxy 和 keepalived
 yum install -y haproxy keepalived
 
 # 配置 haproxy
-cat <<EOF > /etc/haproxy/haproxy.cfg
+cat << EOF > /etc/haproxy/haproxy.cfg
 global
     log /dev/log    local0
     log /dev/log    local1 notice
@@ -25,16 +29,19 @@ defaults
     timeout server 50000
 
 listen redis
-    bind 192.168.100.20:6379
-    bind 192.168.100.21:6379
+    bind *:6379
     mode tcp
     balance roundrobin
-    server redis01 192.168.100.18:6379 check
-    server redis02 192.168.100.19:6379 check
+    server redis01 192.168.100.18:6379 check inter 1000 rise 2 fall 3
+    server redis02 192.168.100.19:6379 check inter 1000 rise 2 fall 3
+    tcp-check send PING\r\n
+    tcp-check expect string +PONG
+    tcp-check send AUTH P@ssw0rd\r\n
+    tcp-check expect string +OK
 EOF
 
 # 配置 keepalived
-cat <<EOF > /etc/keepalived/keepalived.conf
+cat << EOF > /etc/keepalived/keepalived.conf
 global_defs {
     router_id haproxy01
 }
@@ -50,12 +57,8 @@ vrrp_instance VI_1 {
     state MASTER
     virtual_router_id 51
     priority 101
-    authentication {
-        auth_type PASS
-        auth_pass password
-    }
     virtual_ipaddress {
-        192.168.100.50
+        $virtual_ip
     }
     track_script {
         chk_haproxy
@@ -65,6 +68,20 @@ EOF
 
 # 启动 haproxy 和 keepalived
 systemctl enable haproxy
-systemctl start haproxy
 systemctl enable keepalived
+systemctl start haproxy
 systemctl start keepalived
+
+# 配置 haproxy 统计页面
+cat << EOF >> /etc/haproxy/haproxy.cfg
+listen stats
+    bind *:8080
+    mode http
+    stats enable
+    stats uri /
+    stats realm Haproxy\ Statistics
+    stats auth admin:P@ssw0rd
+EOF
+
+# 重启 haproxy
+systemctl restart haproxy
--- haproxy/4.sh
+++ haproxy/4.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # 安装 redis
-yum install -y epel-release
 yum install -y redis
 
 # 配置 redis
-sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis.conf
+sed -i 's/# requirepass foobared/requirepass P@ssw0rd/g' /etc/redis.conf
 
 # 启动 redis
 systemctl enable redis
